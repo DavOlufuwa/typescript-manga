59aaded87791a6428319aeab20b5b2bdb19039e8
Dav.O <davolufuwa@gmail.com>
2023-08-05 02:35:04 +0100

    Added a get chapter hook

---
+++ src/custom-hooks/getChapters.tsx
@@ -0,0 +1,116 @@
+import { useReducer ,useEffect, useCallback} from 'react'
+import axios from "axios"
+
+interface ChapterInterface {
+  provider: string
+  slug: string
+  fullTitle: string
+  shortTitle: string
+  chapterNum: number
+  sourceURL: string
+  shortURL: string
+  chapterNav: {
+    nextSlug: string,
+    nextUrl: string,
+    prevSlug: string,
+    prevUrl: string,
+  }
+  contentURL: string[]
+  createdAt: string
+  updatedAt: string
+}
+
+
+type ChapterStateType = {
+  allChapters: ChapterInterface[]
+  activeChapter: number
+  setActiveChapter: (value: number) => void
+}
+
+const initialState: ChapterStateType = {
+  allChapters: [],
+  activeChapter: 0, 
+  setActiveChapter: () => {},
+}
+
+// Action Types
+type ChapterActions = {
+  type: "setAllChapters" 
+  payload: ChapterInterface[]
+} | {
+  type: "setActiveChapter"
+  payload: number
+}
+// Chapter Reducer
+
+const chapterReducer = (state: ChapterStateType , action: ChapterActions) => {
+  switch (action.type) {
+    case "setAllChapters":
+      return {
+        ...state,
+        allChapters: action.payload
+      }
+    case "setActiveChapter":
+      return {
+        ...state,
+        activeChapter: action.payload
+      }
+    default:
+      return state
+    }
+  }
+
+
+const getChapters = (provider: string, webtoon: string):ChapterStateType  => {
+
+
+
+  const [{allChapters, activeChapter}, dispatch] = useReducer(chapterReducer, initialState)
+
+  const options = {
+    method: 'GET',
+    url: 'https://manga-scrapper.p.rapidapi.com/chapters/all',
+    params: {
+      provider: provider,
+      webtoon: webtoon
+    },
+    headers: {
+      'X-RapidAPI-Key': '76fadf03admshc7d99749ca28ab8p16c57ajsn55c84d8e9759',
+      'X-RapidAPI-Host': 'manga-scrapper.p.rapidapi.com'
+    }
+  };
+
+  useEffect(() => {
+    const fetchChapters = async () => {
+      try {
+        const response = await axios.request(options)
+        
+        dispatch({
+          type: "setAllChapters",
+          payload: response.data
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchChapters()    
+  }, [])
+
+
+  const setActiveChapter = useCallback((value: number) => {
+    dispatch({
+      type: "setActiveChapter",
+      payload: value
+    })
+  }, [])
+
+  return {
+    allChapters,
+    activeChapter,
+    setActiveChapter
+  }
+
+}
+
+export default getChapters
--- src/routes/ComicInfo.tsx
+++ src/routes/ComicInfo.tsx
@@ -1,21 +1,106 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 import { ComicInterface } from '@/custom-hooks/getComics'
+import getChapters from '@/custom-hooks/getChapters'
+import { Button } from '@/components/ui/button'
 
 
 const ComicInfo = () => {
-
   
-  const location = useLocation()
   // getStateFromTheRoute
-  const [loadingManga, setLoadingManga] = useState<boolean>(true)
+  const location = useLocation()
 
   const comicInfo:ComicInterface  =  location.state?.comic
+  
+  // destructure comicInfo
+  const {provider, slug, title, sourceURL, shortURL, coverURL, genre, synopsis, createdAt, updatedAt} = comicInfo
+
+  // getting values from the custom hook
+  const {allChapters, activeChapter, setActiveChapter} = getChapters(provider, slug)
+
+  // loading state
+  const [loadingManga, setLoadingManga] = useState<boolean>(true)
+  
+  // setting values for the pagination
+
+
 
   
 
+  
+
+  
+
+  const sortedChapters = useMemo(() => allChapters.sort((a, b) => a.chapterNum - b.chapterNum), [allChapters])
+
+  const displayedChapter = useMemo(()=> sortedChapters.find(chapter => chapter.chapterNum === activeChapter), [sortedChapters, activeChapter])
+
+  const imageContent: string[] | undefined = displayedChapter?.contentURL
+
+  // function to work with select and option
+  const handleChapterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    
+    const value = Number(event.target.value)
+    
+    setActiveChapter(value)
+  }
+
+  const currentIndex = sortedChapters.findIndex(chapter => chapter.chapterNum === Number(activeChapter))
+
+  // handling chapter change
+  const switchToPrevChapter = () => {
+   if (currentIndex === 0) {
+     return
+   }
+   setActiveChapter(sortedChapters[currentIndex - 1].chapterNum)
+  }
+
+  const switchToNextChapter = () => {
+    if (currentIndex === sortedChapters.length - 1) {
+      return
+    }
+    setActiveChapter(sortedChapters[currentIndex + 1].chapterNum)
+  }
+
+
   return (
-    <div>ComicInfo</div>
+    <main>
+      <h1>{title}</h1>
+      <h2>{provider}</h2>
+
+      <select value={activeChapter} onChange={handleChapterChange}>
+        <option value="">Select A Chapter to Read</option>
+        {
+          sortedChapters.map((chapter, index) => {
+            return (
+              <option key={index} value={chapter.chapterNum}>{chapter.fullTitle}</option>
+            )
+          })
+        }
+      </select>
+      <div>
+        {activeChapter}
+      </div>
+      <div>
+        {
+          imageContent && imageContent.map((image, index) => {
+            return (
+              <img key={index} src={image} alt={title}/>
+            )
+          })
+        }
+      </div>
+
+      <div>
+        <Button
+          onClick={switchToPrevChapter}
+        >Previous Page</Button>
+        <Button
+          onClick={switchToNextChapter}
+        >Next Page</Button>
+      </div>
+
+    </main>
   )
 }
 
