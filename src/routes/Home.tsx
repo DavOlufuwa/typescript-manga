fa14b13226c46e12c3d744c565d176fba3e95d1f
Dav.O <davolufuwa@gmail.com>
2023-08-06 01:10:11 +0100

    styled the latest releases section

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -39,108 +39,123 @@ const Home = () => {
   
   
   return (
-    <div className="px-14 md:px-20 lg:px-32">
-      <h1>All Comics</h1>
-      <section className="relative w-full">
-        <Swiper
-          onBeforeInit={(swiping) => setSwiping(swiping)}
-          speed={700}
-          autoplay={{
-            delay: 5000
-          }}
-          breakpoints={{
-            680: {
-              slidesPerView: 2,
-            },
-            800: {
-              slidesPerView: 3,
-            },
-            1024: {
-              slidesPerView: 4,
-            },
-          }}
-          loop={true}
-          modules={[Autoplay, Controller, Thumbs, Navigation]} 
-        >
-          {
-            allComics.slice(10, 20).map((comic, index) => 
-              <SwiperSlide key={index}>
-                <Link 
-                  to={`/comics/${comic.slug}`}
-                  state={{comic}}
-                >
-                  <div className="h-96 sm:mr-5  ">
-                    <img src={comic.coverURL} alt={`cover of comic with title ${comic.title}`} className="rounded-2xl"/>
-                  </div>
-                </Link>
-              </SwiperSlide>
-            )
-          }
-        </Swiper>
-        
-          <div className="absolute  top-36 -right-14 h-16 w-16 sm:h-20 cursor-pointer" onClick={()=> swiping?.slidePrev()}>
-            <img src={CaretRight} alt="icon to scroll left"/>
-          </div>
-          <div className="absolute top-36 -left-16 sm:-left-20 h-16 w-20 sm:h-20 cursor-pointer" onClick={()=> swiping?.slideNext()}>
-            <img src={CaretLeft} alt="icon to scroll right"/>
-          </div>
-        
-      </section>
-        
-      {/* <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} /> */}
-      {/* <section>
-        <div>
-          Latest Releases
-        </div>
-        <div>
-          {
-            allComics.slice(0, 8).map((comic, index) => 
-              <Card key={index}>
+    <>
+      <div >
+        <h1>All Comics</h1>
 
-
-              </Card>
-            )
-          }
-        </div>
-      </section>
-      <section>
-        {
-          (
-          <div className="grid grid-cols-2">
-            {allComics.map((comic, index) => {
-
-                // destructured each comic
-                const {slug, title, coverURL , genre, } = comic
-                return(
-                  <Link 
-                    key={index} 
-                    to={`/comics/${slug}`}
-                    state={{comic}}
-                  >
-                    <Card>
-                      <CardContent>
-                        <div>
-                          <img src={coverURL} alt={`cover of comic with title ${slug}`}/>
+        {/* First Carousel  */}
+        <div className="px-14 md:px-20 lg:px-32">
+          <section className="relative w-full">
+            <Swiper
+              onBeforeInit={(swiping) => setSwiping(swiping)}
+              speed={700}
+              autoplay={{
+                delay: 5000
+              }}
+              breakpoints={{
+                680: {
+                  slidesPerView: 2,
+                },
+                800: {
+                  slidesPerView: 3,
+                },
+                1024: {
+                  slidesPerView: 4,
+                },
+              }}
+              loop={true}
+              modules={[Autoplay, Controller, Thumbs, Navigation]} 
+            >
+              {
+                allComics.slice(15, 25).map((comic, index) => 
+                  <SwiperSlide key={index}>
+                    <Link 
+                      to={`/comics/${comic.slug}`}
+                      state={{comic}}
+                    >
+                      <div className="parent-cover relative h-96 sm:mr-5 text-white">
+                        <img src={comic.coverURL} alt={`cover of comic with title ${comic.title}`} className="rounded-2xl"/>
+                        
+                        <div className="child-cover">
+                          <p className="mb-5 text-2xl font-semibold text-center">{comic.title}</p>
+                          <p className="mb-6 text-sm font-light">{comic.genre?.slice(0, 3).join(". ")}</p>
                         </div>
-                      </CardContent>
-                      <CardFooter className="flex flex-col">
-                        <div>{title}</div>
-                        <div>{genre?.join(", ")}</div>
-                      </CardFooter>
-                    </Card>
+                      </div>
+                    </Link>
+                  </SwiperSlide>
+                )
+              }
+            </Swiper>
+            
+              <div className="absolute  top-36 -right-14 h-16 w-16 sm:h-20 cursor-pointer" onClick={()=> swiping?.slideNext()}>
+                <img src={CaretRight} alt="icon to scroll left"/>
+              </div>
+              <div className="absolute top-36 -left-16 sm:-left-20 h-16 w-20 sm:h-20 cursor-pointer" onClick={()=> swiping?.slidePrev()}>
+                <img src={CaretLeft} alt="icon to scroll right"/>
+              </div>
+            
+          </section>
+        </div>
+          
+        {/* <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} /> */}
+
+        {/* Latest Releases */}
+        <section className="small-section">
+          <div className="title">
+            Latest Releases
+          </div>
+          <div className="grid md:grid-cols-2 gap-4 md:gap-16">
+            <div className="grid grid-flow-row gap-4">
+              {
+                allComics.slice(0, 4).map((comic, index) => 
+                  <Link
+                    to={`/comics/${comic.slug}`}
+                    state={{comic}}
+                    key={index}
+                  >
+                    <div className="grid grid-cols-4 items-center">
+                      <div className="h-full md:w-20 lg:w-32">
+                        <img src={comic.coverURL} alt={`cover of comic with title ${comic.title}`} className="rounded-xl object-cover "/>
+                      </div>
+                      <div className="text-gray-300 text-3xl text-center font-bold col-auto">
+                        <div>{index + 1}</div>
+                      </div>
+                      <div className="text-gray-200 col-span-2">
+                        <div className="font-bold text-md">{comic.title}</div>
+                        <div className="text-[0.85rem] font-extralight">{comic.genre?.slice(0, 5).join(", ")}</div>
+                      </div>
+                    </div>
                   </Link>
                 )
               }
-            )}
+            </div>
+            <div className="grid gap-4">
+              {
+                allComics.slice(4, 8).map((comic, index) => 
+                  <Link
+                    to={`/comics/${comic.slug}`}
+                    state={{comic}}
+                    key={index}
+                  >
+                    <div className="grid grid-cols-4 items-center">
+                      <div className="md:h-44 md:w-20 lg:w-32">
+                        <img src={comic.coverURL} alt={`cover of comic with title ${comic.title}`} className="rounded-xl object-fill"/>
+                      </div>
+                      <div className="text-gray-300 text-3xl text-center font-bold col-auto">
+                        <div>{index + 5}</div>
+                      </div>
+                      <div className="text-gray-200 col-span-2">
+                        <div className="font-bold text-md">{comic.title}</div>
+                        <div className="text-[0.85rem] font-extralight">{comic.genre?.slice(0, 5).join(", ")}</div>
+                      </div>
+                    </div>
+                  </Link>
+                )
+              }
+            </div>
           </div>
-          )
-        }
-      </section>
-      <section>
-        <div>
-          Recently Viewed
-        </div>
-        <div>
+        </section>
+        {/* <section>
           {
             (
             <div className="grid grid-cols-2">
@@ -172,23 +187,61 @@ const Home = () => {
             </div>
             )
           }
-        </div>
-      </section> */}
-      
-      {/* <div>
-        <ReactPaginate 
-          previousLabel={`${indexOfLastComic > 10 ? "Prev" : ""}`}
-          nextLabel={"Next Page"}
-          breakLabel={"..."}
-          pageCount={Math.ceil(allComics.length / comicsPerPage)}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={3}
-          onPageChange={paginate}
-          containerClassName={"flex items-center justify-center gap-2"}
-          activeClassName="bg-red-100"
-        />
-      </div> */}
-    </div>
+        </section> */}
+        {/* <section>
+          <div>
+            Recently Viewed
+          </div>
+          <div>
+            {
+              (
+              <div className="grid grid-cols-2">
+                {allComics.map((comic, index) => {
+
+                    // destructured each comic
+                    const {slug, title, coverURL , genre, } = comic
+                    return(
+                      <Link 
+                        key={index} 
+                        to={`/comics/${slug}`}
+                        state={{comic}}
+                      >
+                        <Card>
+                          <CardContent>
+                            <div>
+                              <img src={coverURL} alt={`cover of comic with title ${slug}`}/>
+                            </div>
+                          </CardContent>
+                          <CardFooter className="flex flex-col">
+                            <div>{title}</div>
+                            <div>{genre?.join(", ")}</div>
+                          </CardFooter>
+                        </Card>
+                      </Link>
+                    )
+                  }
+                )}
+              </div>
+              )
+            }
+          </div>
+        </section> */}
+        
+        {/* <div>
+          <ReactPaginate 
+            previousLabel={`${indexOfLastComic > 10 ? "Prev" : ""}`}
+            nextLabel={"Next Page"}
+            breakLabel={"..."}
+            pageCount={Math.ceil(allComics.length / comicsPerPage)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={paginate}
+            containerClassName={"flex items-center justify-center gap-2"}
+            activeClassName="bg-red-100"
+          />
+        </div> */}
+      </div>
+    </>
   )
 }
 
